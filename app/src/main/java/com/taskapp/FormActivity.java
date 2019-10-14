f194a7b5c27f90083100c0202c4854f1a5f13e70
NurdinBakeev <Vilyavilovich@gmail.com>
2019-10-14 15:19:42 +0300

    sh

---
+++ app/src/main/java/com/taskapp/FormActivity.java
@@ -0,0 +1,43 @@
+package com.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+public class FormActivity extends AppCompatActivity {
+
+    EditText editTitle, editDesc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        editTitle = findViewById(R.id.editTitle);
+        editDesc = findViewById(R.id.editDesk);
+    }
+
+    public void onClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String desc = editDesc.getText().toString().trim();
+
+        if (TextUtils.isEmpty(title)){
+            editTitle.setError("Пустые поля не принимаем");
+            return;
+        }
+
+        if (TextUtils.isEmpty(desc)){
+            editDesc.setError("Пустые поля не принимаем");
+            return;
+        }
+
+        Task task = new Task(title,desc);
+        Intent intent = new Intent();
+        intent.putExtra("task", task);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
